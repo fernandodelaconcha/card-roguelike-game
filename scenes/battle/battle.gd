d8dc652895d2819308e7d8c13a01bd21e2ba868e
Fernando de la Concha <fernandodelaconcha@gmail.com>
2024-07-13 12:00:43 +0200

    add draw and discard mechanisms

---
+++ scenes/battle/battle.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var char_stats: CharacterStats
+
+@onready var battle_ui: CanvasLayer = $BattleUI
+@onready var player_handler: Node = $PlayerHandler
+@onready var player: Player = $Player as Player
+
+func _ready() -> void:
+	var new_stats: CharacterStats = char_stats.create_instance()
+	battle_ui.char_stats = new_stats
+	player.stats = new_stats
+	
+	Events.player_turn_ended.connect(player_handler.end_turn)
+	Events.player_hand_discarded.connect(player_handler.start_turn)
+	
+	start_battle(new_stats)
+
+func start_battle(stats: CharacterStats) -> void:
+	player_handler.start_battle(stats)
